8aa4a5243123e7c20ce0c82d327d07f5647efcf1
vanajabobby86 <ivanamax23@gmail.com>
2023-02-23 19:06:24 +0700

    test

---
+++ src/org/phone/ExternalStorage.java
@@ -0,0 +1,14 @@
+package org.phone;
+
+public class ExternalStorage {
+	public void size() {
+		System.out.println("extrenal storage size");}
+	public static void main(String[] args) {
+		ExternalStorage capacity=new ExternalStorage();
+		capacity.size();
+		
+	}
+
+	}
+
+
---
+++ src/org/phone/InternalStorage.java
@@ -0,0 +1,21 @@
+package org.phone;
+
+public class InternalStorage {
+	private void processorName() {
+		System.out.println("enter processor name");}
+	private void ramSize() {
+		System.out.println("enter ram size");}
+public static void main(String[] args) {
+	InternalStorage process=new InternalStorage();
+	process.processorName();
+	process.ramSize();
+	ExternalStorage capacity=new ExternalStorage();
+	capacity.size();
+}
+	}
+	
+		// TODO Auto-generated method stub
+
+	
+
+
